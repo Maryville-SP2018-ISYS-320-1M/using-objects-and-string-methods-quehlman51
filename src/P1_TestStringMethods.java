a1fa54d84b227a1753c55150d6ca4f044441ccdc
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 00:16:26 -0500

    Input predictions

--- src/P1_TestStringMethods.java
+++ src/P1_TestStringMethods.java
@@ -7,19 +7,19 @@
 /*
  Your expression predictions:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. 13
+ 2. a
+ 3. G
+ 4. 2, 4
  
- 5.
- 6.
- 7.
- 8.
+ 5. GANDALF THE GRAY
+ 6. frodo baggins
+ 7. o Baggins
+ 8. dalf the GR
  
- 9.
- 10.
- 11.
+ 9. Goondoolf the GRAY
+ 10. Error/Nothing
+ 11. strange1
  
   
  */
@@ -44,6 +44,8 @@
  */
 public class P1_TestStringMethods {
 	public static void main(String[] args) {
+		String str1 = "Frodo Baggins";
+		String str2 = "Gandalf the GRAY";
 		System.out.println("REPLACE WITH YOUR EXPRESSION");
 	}
 
