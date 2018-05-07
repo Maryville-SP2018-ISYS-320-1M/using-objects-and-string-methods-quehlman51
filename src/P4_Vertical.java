8b7923c3fc6bd511f2e378e21100fdd7ffcd21cb
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 00:40:53 -0500

    Initialized variable and created method

--- src/P4_Vertical.java
+++ src/P4_Vertical.java
@@ -1,13 +1,20 @@
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
 public class P4_Vertical {
 
 	public static void main(String[] args) {
+		String string = "hey now";
+		vertical(string);
+	}
 
+	public static void vertical(String string) {
+		for (int i = 0; i < string.length(); i++) {
+			System.out.println(string.charAt(i));
+		}
 	}
 
 }
