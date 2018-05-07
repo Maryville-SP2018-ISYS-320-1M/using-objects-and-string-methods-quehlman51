e50733dec289908df36200749d3f59ff9e611958
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 00:32:12 -0500

    Created string method

--- src/P2_BananaStand.java
+++ src/P2_BananaStand.java
@@ -1,6 +1,6 @@
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
@@ -8,6 +8,7 @@
 public class P2_BananaStand {
 	public static void main(String[] args) {
 		String quote = "There's always money in the banana stand.";
+		System.out.print(quote.toUpperCase().substring(28, 34));
 		
 	}
 }
