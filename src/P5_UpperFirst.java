01bcce47f86ee29f79cee017ba3da3e355c1ac4b
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 00:43:33 -0500

    Created method

--- src/P5_UpperFirst.java
+++ src/P5_UpperFirst.java
@@ -1,13 +1,26 @@
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
 public class P5_UpperFirst {
 
 	public static void main(String[] args) {
-
+		String word = "Jedi Knight";
+		String delimiter = " ";
+		
+		upperFirst(word, delimiter);
+		System.out.print(upperFirst(word, delimiter));
+	}
+	
+	public static String upperFirst(String word, String delimiter) {
+		String[] halves = word.split(delimiter);
+		String firstHalf = halves[0];
+		String secondHalf = halves[1];
+		String updatedString = firstHalf.toUpperCase() + delimiter + secondHalf;
+		
+		return(updatedString);
 	}
 
 }
