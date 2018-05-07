c27864a8c7656c7d7d42858356722d5a3437d553
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 00:39:25 -0500

    Created method

--- src/P2_BananaStand.java
+++ src/P2_BananaStand.java
@@ -8,7 +8,14 @@
 public class P2_BananaStand {
 	public static void main(String[] args) {
 		String quote = "There's always money in the banana stand.";
-		System.out.print(quote.toUpperCase().substring(28, 34));
+		System.out.println(quote.toUpperCase().substring(28, 34));
+		lastHalf("There's always money in the banana stand.");
+	}
+	
+	public static String lastHalf(String quote) {
+		int half = quote.length() / 2;
+		String newQuote = quote.substring(half);
+		return newQuote;
 		
 	}
 }
